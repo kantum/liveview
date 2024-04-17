b5d19b8933e818e69ff688cee30bdbd86e3af25c
kantum <42kantum@gmail.com>
2024-04-17 18:30:48 +0200

    fix: move json example to hello

---
+++ lib/liveview_web/controllers/hello_json.ex
@@ -0,0 +1,5 @@
+defmodule LiveviewWeb.HelloJSON do
+  def index(_assigns) do
+    %{message: "this is some json"}
+  end
+end
--- lib/liveview_web/controllers/page_json.ex
+++
@@ -1,5 +0,0 @@
-defmodule LiveviewWeb.PageJSON do
-  def home(_assigns) do
-    %{message: "this is some json"}
-  end
-end
